2d8121efb8f2fbf4f85571b5691ccf2e421ed88a
Andre <santtosandre7@gmail.com>
2025-11-16 11:30:03 -0300

    adicionadas classes de security

---
+++ src/main/java/usuario/infraestructure/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package usuario.infraestructure.repository;
+
+import aprendendospring.infraestructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
