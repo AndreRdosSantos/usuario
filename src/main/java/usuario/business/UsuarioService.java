a328a4baf95d87c53c7641355c017f7bfa26aa42
Andre <santtosandre7@gmail.com>
2025-11-16 23:51:17 -0300

    adicionados metodos get, delete e login

--- src/main/java/usuario/business/UsuarioService.java
+++ src/main/java/usuario/business/UsuarioService.java
@@ -1,10 +1,14 @@
 package usuario.business;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import usuario.business.converter.UsuarioConverter;
 import usuario.business.dto.UsuarioDTO;
 import usuario.infraestructure.entity.Usuario;
+import usuario.infraestructure.exceptions.ConflictException;
+import usuario.infraestructure.exceptions.ResourceNotFoundException;
 import usuario.infraestructure.repository.UsuarioRepository;
 
 @Service
@@ -12,13 +16,40 @@ import usuario.infraestructure.repository.UsuarioRepository;
 public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
+    private final PasswordEncoder passwordEncoder;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
+        emailExiste(usuarioDTO.getEmail());
+        usuarioDTO.setSenha(passwordEncoder.encode(usuarioDTO.getSenha()));
         Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO);
         return usuarioConverter.paraUsuarioDTO(
                 usuarioRepository.save(usuario)
         );
     }
 
+    public void emailExiste(String email){
+        try {
+            boolean existe = verificaEmailExistente(email);
+            if (existe) {
+                throw new ConflictException("Email já cadastrado " + email);
+            }
+        } catch (ConflictException e) {
+            throw new ConflictException("Email já cadastrado ", e.getCause());
+        }
+    }
+
+    public boolean verificaEmailExistente(String email){
+        return usuarioRepository.existsByEmail(email);
+    }
+
+    public Usuario buscarUsuarioPorEmail(String email){
+        return usuarioRepository.findByEmail(email).orElseThrow(
+                () -> new ResourceNotFoundException("Email não encontrado " + email)
+        );
+    }
+
+    public void deletaUsuarioPorEmail(String email){
+        usuarioRepository.deleteByEmail(email);
+    }
 
 }
--- src/main/java/usuario/infraestructure/repository/EnderecoRepository.java
+++ src/main/java/usuario/infraestructure/repository/EnderecoRepository.java
@@ -1,8 +1,9 @@
 package usuario.infraestructure.repository;
 
-import aprendendospring.infraestructure.entity.Endereco;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import usuario.infraestructure.entity.Endereco;
 
 @Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
