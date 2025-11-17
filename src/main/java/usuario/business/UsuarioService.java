dc18b87662c4a2975d221ae8c92adab1f8fcf4f5
Andre <santtosandre7@gmail.com>
2025-11-17 12:33:30 -0300

    inclusão de metodo put de usuario

--- src/main/java/usuario/business/UsuarioService.java
+++ src/main/java/usuario/business/UsuarioService.java
@@ -10,6 +10,7 @@ import usuario.infraestructure.entity.Usuario;
 import usuario.infraestructure.exceptions.ConflictException;
 import usuario.infraestructure.exceptions.ResourceNotFoundException;
 import usuario.infraestructure.repository.UsuarioRepository;
+import usuario.infraestructure.security.JwtUtil;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +18,7 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
         emailExiste(usuarioDTO.getEmail());
@@ -50,6 +52,17 @@ public class UsuarioService {
 
     public void deletaUsuarioPorEmail(String email){
         usuarioRepository.deleteByEmail(email);
+    }
+
+    public UsuarioDTO atualizaDadosUsuario(String token, UsuarioDTO dto){
+        String email = jwtUtil.extrairEmailToken(token.substring(7));
+        dto.setSenha(dto.getSenha() != null ? passwordEncoder.encode(dto.getSenha()) : null);
+        Usuario usuarioEntity = usuarioRepository.findByEmail(email).orElseThrow(
+                () -> new ResourceNotFoundException("Email não encontrado")
+        );
+        Usuario usuario = usuarioConverter.updateUsuario(dto, usuarioEntity);
+
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
     }
 
 }
